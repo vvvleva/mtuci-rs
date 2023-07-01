17f0b95469d29ca4674cd3fb80a320bd190f71dc
vvvleva <124389416+vvvleva@users.noreply.github.com>
2023-07-01 21:57:00 +0300

    Create Code3.rs

---
+++ RustBook/Chapter3/Code3.rs
@@ -0,0 +1,17 @@
+fn main() {
+    // addition
+    let _sum = 5 + 10;
+
+    // subtraction
+    let _difference = 95.5 - 4.3;
+
+    // multiplication
+    let _product = 4 * 30;
+
+    // division
+    let _quotient = 56.7 / 32.2;
+    let _truncated = -5 / 3; // Results in -1
+
+    // remainder
+    let _remainder = 43 % 5;
+}
