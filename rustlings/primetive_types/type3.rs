000633facfa7766fa4a5001d93fcddded89ae9b6
vvvleva <124389416+vvvleva@users.noreply.github.com>
2023-07-06 20:27:58 +0300

    Create type3.rs

---
+++ rustlings/primetive_types/type3.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let a: [i32; 5] = [10; 5];
+
+    if a.len() >= 100 {
+        println!("Wow, that's a big array!");
+    } else {
+        println!("Meh, I eat arrays like that for breakfast.");
+    }
+}
