14920f8fc90b13460911215c5c02330e40e355ac
vvvleva <124389416+vvvleva@users.noreply.github.com>
2023-07-06 21:05:19 +0300

    Create type6.rs

---
+++ rustlings/primetive_types/type6.rs
@@ -0,0 +1,9 @@
+#[test]
+fn indexing_tuple() {
+    let numbers = (1, 2, 3);
+    // Replace below ??? with the tuple indexing syntax.
+    let second = (2);
+
+    assert_eq!(2, second,
+        "This is not the 2nd number in the tuple!")
+}
