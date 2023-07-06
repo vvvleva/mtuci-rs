7b98724d1b7fedbc03aa3c31937c1e54fde9682d
vvvleva <124389416+vvvleva@users.noreply.github.com>
2023-07-06 20:15:52 +0300

    Create type2.rs

---
+++ rustlings/primetive_types/type2.rs
@@ -0,0 +1,23 @@
+fn main() {
+    // Characters (`char`)
+
+    // Note the _single_ quotes, these are different from the double quotes
+    // you've been seeing around.
+    let my_first_initial = 'C';
+    if my_first_initial.is_alphabetic() {
+        println!("Alphabetical!");
+    } else if my_first_initial.is_numeric() {
+        println!("Numerical!");
+    } else {
+        println!("Neither alphabetic nor numeric!");
+    }
+
+    let your_character= 'я';
+    if your_character.is_alphabetic() {
+        println!("Alphabetical!");
+    } else if your_character.is_numeric() {
+        println!("Numerical!");
+    } else {
+        println!("Neither alphabetic nor numeric!");
+    }
+}
