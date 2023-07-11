9028598a027dfc5d4e14fc0122082eeb9535f55e
vvvleva <124389416+vvvleva@users.noreply.github.com>
2023-07-11 17:24:43 +0300

    Create trait5.rs

---
+++ rustlings/traits/trait5.rs
@@ -0,0 +1,29 @@
+pub trait SomeTrait {
+    fn some_function(&self) -> bool {
+        true
+    }
+}
+
+pub trait OtherTrait {
+    fn other_function(&self) -> bool {
+        true
+    }
+}
+
+struct SomeStruct {}
+struct OtherStruct {}
+
+impl SomeTrait for SomeStruct {}
+impl OtherTrait for SomeStruct {}
+impl SomeTrait for OtherStruct {}
+impl OtherTrait for OtherStruct {}
+
+// YOU MAY ONLY CHANGE THE NEXT LINE
+fn some_func(item: impl SomeTrait+OtherTrait) -> bool {
+    item.some_function() && item.other_function()
+}
+
+fn main() {
+    some_func(SomeStruct {});
+    some_func(OtherStruct {});
+}
