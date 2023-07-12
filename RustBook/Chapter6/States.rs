4b836afbd806f0315c346c2a643ececbaa904b52
vvvleva <124389416+vvvleva@users.noreply.github.com>
2023-07-12 20:31:21 +0300

    Update States.rs

--- RustBook/Chapter6/States.rs
+++ RustBook/Chapter6/States.rs
@@ -27,3 +27,10 @@ fn value_in_cents(coin: Coin) -> u8 {
 fn main() {
     value_in_cents(Coin::Quarter(UsState::Alaska));
 }
+
+
+
+//Если мы сделаем вызов функции value_in_cents(Coin::Quarter(UsState::Alaska)), то coin будет иметь значение Coin::Quarter(UsState::Alaska).
+//Когда мы будем сравнивать это значение с каждой из веток, ни одна из них не будет совпадать, пока мы не достигнем варианта Coin::Quarter(state).
+//В этот момент state привяжется к значению UsState::Alaska.
+//Затем мы сможем использовать эту привязку в выражении println!, получив таким образом внутреннее значение варианта Quarter перечисления Coin.
