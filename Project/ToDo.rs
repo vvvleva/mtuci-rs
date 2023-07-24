c3fc5b03c0518dbf93ce82dcbdf126bd9633fdd1
vvvleva <124389416+vvvleva@users.noreply.github.com>
2023-07-24 14:56:07 +0300

    Update ToDo.rs

--- Project/ToDo.rs
+++ Project/ToDo.rs
@@ -10,12 +10,32 @@ use druid::widget::Flex;
 use druid::widget::Button;
 use druid::widget::TextBox;
 use druid::WidgetExt;
+use druid::Env;
+use druid::widget::Checkbox;
+use druid::widget::List;
 
 pub fn ui_builder() -> impl Widget <TodoState>{
     let header = Flex::row()
-    .with_child(TextBox::new().lens(TodoState::new_text))
-    .with_child(Button::new("->"));
-    Flex::column().with_child(header)
+    .with_flex_child(TextBox::new()
+        .lens(TodoState::new_text)
+        .expand_width(), 1.)
+    .with_child(
+        Button::new("->")
+        .on_click(|_ctx, data: &mut TodoState, _env|{
+            if data.new_text.trim() !=""{
+                let text = data.new_text.clone();
+                data.new_text = "". to_string();
+                data.todos.push_front(TodoItem{ checked: false, text })
+            }
+        }));
+
+    let todos = List::new(|| {
+        Flex::row()
+        .with_child(Checkbox::new("").lens(TodoItem::checked))
+        .with_child(Label::new(|data: &TodoItem, _: &Env| data.text.clone()))
+    }).lens(TodoState::todos);
+
+    Flex::column().with_child(header).with_child(todos)
 }
 
 fn main(){
